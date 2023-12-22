49c19e389f75e7f6ac047774c53f779844195af8
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-12-22 11:01:09 +0900

    Add: lazyvim: add bash ls

--- dotfiles/lazyvim/lua/config/lazy.lua
+++ dotfiles/lazyvim/lua/config/lazy.lua
@@ -25,7 +25,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.docker" },
