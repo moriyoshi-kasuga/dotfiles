b031fecf87c73bfb87da74c3e6e8ed72737ab517
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-12-27 15:52:13 +0900

    feat(lazyvim): add formatters prepend args :sparkles:

--- dotfiles/lazyvim/lua/plugins/lsp.lua
+++ dotfiles/lazyvim/lua/plugins/lsp.lua
@@ -35,6 +35,17 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        djlint = {
+          prepend_args = { "--indent", "2" },
+        },
+        black = {
+          prepend_args = { "--fast" },
+        },
+        flake8 = {
+          prepend_args = { "--max-line-length", "88" },
+        },
+      },
       formatters_by_ft = {
         ["htmldjango"] = { "djlint" },
       },
