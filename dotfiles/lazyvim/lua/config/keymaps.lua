b79bdfbe7a2811e9d38b70f00470e152b0a48713
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-12-29 15:08:10 +0900

    refactor(lazyvim): set keymaps

--- dotfiles/lazyvim/lua/config/keymaps.lua
+++ dotfiles/lazyvim/lua/config/keymaps.lua
@@ -8,34 +8,19 @@ keymap.set("n", "-", "<C-x>")
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
+-- leave insert mode
 keymap.set("i", "jk", "<esc>")
 
-keymap.del("n", "<leader>xl")
-keymap.del("n", "<leader>xq")
-
-keymap.del("n", "[q")
-keymap.del("n", "]q")
-
--- LazyVim Changelog
-keymap.del("n", "<leader>L")
-
--- floating terminal
-keymap.del("n", "<leader>ft")
-keymap.del("n", "<leader>fT")
-
--- Terminal Mappings
+-- delete Terminal Mappings
 keymap.del("t", "<C-h>")
 keymap.del("t", "<C-j>")
 keymap.del("t", "<C-k>")
 keymap.del("t", "<C-l>")
 
 -- windows
-keymap.del("n", "<leader>w-")
-keymap.del("n", "<leader>w|")
-keymap.del("n", "<leader>-")
-keymap.del("n", "<leader>|")
-keymap.set("n", "<leader>ws", "<C-W>s", { desc = "Split window horizontal"})
-keymap.set("n", "<leader>wv", "<C-W>v", { desc = "Split window vertical"})
+keymap.set("n", "<leader>ws", "<C-W>s", { desc = "Split window horizontal" })
+keymap.set("n", "<leader>wv", "<C-W>v", { desc = "Split window vertical" })
+
 -- codeium
 keymap.set("i", "<c-f>", function()
   return vim.fn["codeium#Accept"]()
@@ -44,6 +29,12 @@ keymap.set("i", "<c-x>", function()
   return vim.fn["codeium#Clear"]()
 end, { expr = true, silent = true })
 -- format
-keymap.set("n","<leader>r",function()
+keymap.set("n", "<leader>r", function()
   Util.format({ force = true })
-end,{desc="format"})
+end, { desc = "format" })
+
+-- buffers
+keymap.del("n", "<S-h>")
+keymap.del("n", "<S-l>")
+keymap.set("n","<A-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer" })
+keymap.set("n","<A-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
--- dotfiles/lazyvim/lua/plugins/telescope.lua
+++ dotfiles/lazyvim/lua/plugins/telescope.lua
@@ -10,29 +10,9 @@ return {
         winblend = 0,
       },
     },
-  },
-
-  {
-    "telescope.nvim",
-    dependencies = {
-      -- project management
-      {
-        "ahmedkhalf/project.nvim",
-        opts = {
-          manual_mode = true,
-        },
-        event = "VeryLazy",
-        config = function(_, opts)
-          require("project_nvim").setup(opts)
-          require("lazyvim.util").on_load("telescope.nvim", function()
-            require("telescope").load_extension("projects")
-          end)
-        end,
-        keys = {
-          { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
-        },
-      },
-    },
+    keys = {
+      {"<leader>sb","<cmd>Telescope buffers<cr>", desc = "Search buffer"},
+    }
   },
 
   -- add telescope-fzf-native
