909b5d0346008b8b13e9d12a8e5704371c082a49
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-01-26 19:28:54 +0900

    feat(zsh): fzf history function to pull in arguments :sparkles:

--- dotfiles/zsh/commands.zsh
+++ dotfiles/zsh/commands.zsh
@@ -5,7 +5,7 @@ source "${HOME}/zsh/function/venv.zsh"
 source "${HOME}/zsh/function/tmux.zsh"
 
 function fzf-history-selection() {
-    BUFFER=`history -n 1 | tac  | awk '!a[$0]++' | grep -v 'cd ' | fzf --reverse`
+    BUFFER=`history -n 1 | tac  | awk '!a[$0]++' | grep -v 'cd ' | fzf --reverse --prompt="history >" --query "$LBUFFER"`
     CURSOR=$#BUFFER
     zle reset-prompt
 }
