4158e12da8c65559cd3785557f88687a9d22d5a7
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-04-28 22:06:26 +0900

    feat(neovim): add open_term and copy_path features to neo tree :sparkles:

--- dotfiles/nvim/lua/plugins/ui/neotree.lua
+++ dotfiles/nvim/lua/plugins/ui/neotree.lua
@@ -59,6 +59,62 @@ return {
             state.commands.open(state)
           end
         end,
+        open_term = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          require("toggleterm.terminal").Terminal
+            :new({
+              dir = node.type == "directory" and path or string.match(path, "(.-)/[^/]*$"),
+              direction = "float",
+              float_opts = {
+                border = "double",
+              },
+              on_open = function(open)
+                vim.cmd("startinsert!")
+                vim.api.nvim_buf_set_keymap(open.bufnr, "t", "<c-_>", "<cmd>close<CR>", { silent = true })
+              end,
+            })
+            :toggle()
+        end,
+        copy_path = function(state)
+          -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+          -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local results = {
+            filepath,
+            modify(filepath, ":."),
+            modify(filepath, ":~"),
+            filename,
+            modify(filename, ":r"),
+            modify(filename, ":e"),
+          }
+
+          vim.ui.select({
+            "1. Absolute path: " .. results[1],
+            "2. Path relative to CWD: " .. results[2],
+            "3. Path relative to HOME: " .. results[3],
+            "4. Filename: " .. results[4],
+            "5. Filename without extension: " .. results[5],
+            "6. Extension of the filename: " .. results[6],
+          }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+            if choice then
+              local i = tonumber(choice:sub(1, 1))
+              if i then
+                local result = results[i]
+                vim.fn.setreg("*", result)
+                vim.notify("Copied: " .. result)
+              else
+                vim.notify("Invalid selection")
+              end
+            else
+              vim.notify("Selection cancelled")
+            end
+          end)
+        end,
       },
       filesystem = {
         filtered_items = {
@@ -81,12 +137,12 @@ return {
         mappings = {
           ["<space>"] = "none",
           ["Y"] = {
-            function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg("+", path, "c")
-            end,
+            "copy_path",
             desc = "Copy Path to Clipboard",
+          },
+          ["t"] = {
+            "open_term",
+            desc = "Open term of under folder",
           },
           ["O"] = {
             function(state)
