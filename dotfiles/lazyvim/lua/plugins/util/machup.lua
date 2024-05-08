e615cd466016a9f4519404ef481537c328058552
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-05-08 21:08:03 +0900

    perf(lazyvim): remove vim-mathup became lag :rocket:

--- dotfiles/lazyvim/lua/plugins/util/machup.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "andymass/vim-matchup",
-  config = function()
-    -- may set any options here
-    vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    vim.g.matchup_matchparen_pumvisible = 0
-  end,
-}
