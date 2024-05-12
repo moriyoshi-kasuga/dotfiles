801f816b24402dde97c0457bff352d56376f1919
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-05-12 19:31:30 +0900

    feat(lazyvim): change context plugin to nvim-navic :sparkles:

--- dotfiles/lazyvim/lua/config/lazy.lua
+++ dotfiles/lazyvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.codeium" },
 
     { import = "lazyvim.plugins.extras.editor.outline" },
+    { import = "lazyvim.plugins.extras.editor.navic" },
 
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.black" },
--- dotfiles/lazyvim/lua/plugins/disable.lua
+++ dotfiles/lazyvim/lua/plugins/disable.lua
@@ -2,4 +2,5 @@ return {
   { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.comment", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
 }
