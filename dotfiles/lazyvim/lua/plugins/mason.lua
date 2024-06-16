7666a49bbe18a8311e958340520acc1b14974b21
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-06-16 21:58:29 +0900

    refactor(lazyvim): remove dead code. :recycle:

--- dotfiles/lazyvim/lua/plugins/mason.lua
+++ dotfiles/lazyvim/lua/plugins/mason.lua
@@ -2,23 +2,9 @@ return {
   "williamboman/mason.nvim",
   opts = function(_, opts)
     vim.list_extend(opts.ensure_installed, {
-      "black",
-      "prettier",
       "stylua",
       "selene",
       "luacheck",
-      "shellcheck",
-      "shfmt",
-      "tailwindcss-language-server",
-      "typescript-language-server",
-      "html-lsp",
-      "css-lsp",
-      "bash-language-server",
-      "hadolint",
-      "flake8",
-      "djlint",
-      "java-test",
-      "java-debug-adapter",
     })
   end,
   keys = {
