618ac2359262c70d74353739de199df64f0a946b
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-09-12 22:46:30 +0900

    fix(lazyvim): remove emmet-ls

--- dotfiles/lazyvim/lua/plugins/lang/html-css.lua
+++ dotfiles/lazyvim/lua/plugins/lang/html-css.lua
@@ -13,7 +13,7 @@ return {
     optional = true,
     opts = function(_, opts)
       if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "html-lsp", "css-lsp", "emmet-ls" })
+        vim.list_extend(opts.ensure_installed, { "html-lsp", "css-lsp" })
       end
     end,
   },
