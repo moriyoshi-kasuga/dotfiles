317f2743cdd079d6a36d9bb456723dcbaf381f53
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-11-24 00:13:06 +0900

    feat(lazyvim): migrate to 7

--- dotfiles/lazyvim/lua/config/lazy.lua
+++ dotfiles/lazyvim/lua/config/lazy.lua
@@ -24,10 +24,9 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.editor.illuminate" },
 
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.ai.codeium" },
 
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
     { import = "lazyvim.plugins.extras.linting.eslint" },
 
