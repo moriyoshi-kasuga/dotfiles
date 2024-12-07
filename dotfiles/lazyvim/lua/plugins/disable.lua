b8ab0db03ebac917fc246214a267cc6ada7351e8
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-12-07 20:49:03 +0900

    feat(lazyvim): enable trouble and remove disabled plugin of Not currently in use

--- dotfiles/lazyvim/lua/plugins/disable.lua
+++ dotfiles/lazyvim/lua/plugins/disable.lua
@@ -1,9 +1,7 @@
 return {
-  { "folke/trouble.nvim", enabled = false },
   { "garymjr/nvim-snippets", enabled = false },
   { "friendly-snippets", enabled = false },
   -- TODO: replace to https://github.com/stevearc/oil.nvim
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
 }
