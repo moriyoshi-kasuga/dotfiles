371d38350141eb9cd1d90760356b408befea7deb
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-12-21 18:24:04 +0900

    fix(lazyvim): remove unneeded

--- dotfiles/lazyvim/lua/plugins/cmp.lua
+++ dotfiles/lazyvim/lua/plugins/cmp.lua
@@ -3,24 +3,13 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
-        accept = { auto_brackets = { enabled = true } },
         menu = {
           border = "single",
           scrolloff = 1,
           scrollbar = false,
-          columns = { { "kind_icon" }, { "label", "label_description", gap = 0 } },
-
-          draw = {
-            padding = 0,
-            gap = 1,
-            treesitter = true,
-          },
         },
 
         documentation = {
-          auto_show_delay_ms = 0,
-          auto_show = true,
-
           window = {
             border = "single",
           },
