1c6e8f87c2d31c94694656f9df2e006adba77070
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-01-25 19:29:54 +0900

    fix(lazyvim): enable necessary style on lualine

--- dotfiles/lazyvim/lua/plugins/ui/lualine.lua
+++ dotfiles/lazyvim/lua/plugins/ui/lualine.lua
@@ -1,13 +1,6 @@
-local function maximize_status()
-  return vim.t.maximized and "   " or ""
-end
-
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    local x = opts.sections.lualine_x[#opts.sections.lualine_x]
-    opts.sections.lualine_x = { maximize_status, x }
     opts.sections.lualine_c[#opts.sections.lualine_c] = { LazyVim.lualine.pretty_path({ length = 5 }) }
-    opts.sections.lualine_z = {}
   end,
 }
