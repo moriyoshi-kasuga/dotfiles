aed64abe433640618e2c6b4d32e080a6cf25f1c8
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-15 12:41:34 +0900

    fix(lazyvim): fix lualine style

--- dotfiles/lazyvim/lua/plugins/ui/lualine.lua
+++ dotfiles/lazyvim/lua/plugins/ui/lualine.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = function(_, opts)
-    opts.sections.lualine_c[#opts.sections.lualine_c] = { LazyVim.lualine.pretty_path({ length = 5 }) }
+    opts.sections.lualine_c[#opts.sections.lualine_c - 1] = { LazyVim.lualine.pretty_path({ length = 5 }) }
   end,
 }
