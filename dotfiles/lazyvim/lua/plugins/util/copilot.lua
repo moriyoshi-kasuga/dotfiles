8b1e5185a1e72d0ba6300fc8d72839ececf7d006
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-02-26 15:08:56 +0900

    fix(lazyvim): disable cmp in blink.cmp

--- dotfiles/lazyvim/lua/plugins/util/copilot.lua
+++ dotfiles/lazyvim/lua/plugins/util/copilot.lua
@@ -1,14 +1,41 @@
 return {
+  -- copilot
   {
-    "saghen/blink.cmp",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
     opts = {
-      sources = {
-        providers = {
-          copilot = {
-            score_offset = -100,
-          },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = "<Tab>",
+          next = "<M-]>",
+          prev = "<M-[>",
         },
       },
     },
+  },
+
+  -- ref: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/ai/copilot.lua#L42
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(
+        opts.sections.lualine_x,
+        2,
+        LazyVim.lualine.status(LazyVim.config.icons.kinds.Copilot, function()
+          local clients = package.loaded["copilot"] and LazyVim.lsp.get_clients({ name = "copilot", bufnr = 0 }) or {}
+          if #clients > 0 then
+            local status = require("copilot.api").status.data.status
+            return (status == "InProgress" and "pending") or (status == "Warning" and "error") or "ok"
+          end
+        end)
+      )
+    end,
   },
 }
