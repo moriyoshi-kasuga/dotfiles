eb49804ffca1745fdf24889a33380e67e53574c0
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-03-29 15:23:19 +0900

    fix(lazyvim): disable overlap completion of ai and blink.cmp

--- dotfiles/lazyvim/lua/config/autocmds.lua
+++ dotfiles/lazyvim/lua/config/autocmds.lua
@@ -30,3 +30,18 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuOpen",
+  callback = function()
+    require("copilot.suggestion").dismiss()
+    vim.b.copilot_suggestion_hidden = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "BlinkCmpMenuClose",
+  callback = function()
+    vim.b.copilot_suggestion_hidden = false
+  end,
+})
