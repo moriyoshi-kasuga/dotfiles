2bd40fe358c8d69aef59614780e3c9d93e91e1e6
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-04-19 00:10:33 +0900

    fix(lazyvim): override filetypes on copilot

--- dotfiles/lazyvim/lua/config/autocmds.lua
+++ dotfiles/lazyvim/lua/config/autocmds.lua
@@ -30,18 +30,3 @@ vim.api.nvim_create_autocmd("FileType", {
     end)
   end,
 })
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuOpen",
-  callback = function()
-    require("copilot.suggestion").dismiss()
-    vim.b.copilot_suggestion_hidden = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "BlinkCmpMenuClose",
-  callback = function()
-    vim.b.copilot_suggestion_hidden = false
-  end,
-})
