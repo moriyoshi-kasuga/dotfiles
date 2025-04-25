2ae4d17e8f7fbf49323d0be99f8a30cc14cb3cc9
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-04-25 18:43:13 +0900

    fix(lazyvim): enable disabled plugin

--- dotfiles/lazyvim/lua/plugins/disable.lua
+++ dotfiles/lazyvim/lua/plugins/disable.lua
@@ -1,8 +1,5 @@
 return {
-  { "garymjr/nvim-snippets", enabled = false },
-  { "friendly-snippets", enabled = false },
   -- TODO: replace to https://github.com/stevearc/oil.nvim
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
 }
