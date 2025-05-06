fb62aafa32d0b56ebb71c4ccd88e9fd830452970
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-06 14:25:14 +0900

    feat(zsh): prioritize llvm on mac

--- dotfiles/zsh/darwin.zsh
+++ dotfiles/zsh/darwin.zsh
@@ -4,3 +4,8 @@ export ANDROID_NDK_HOME=$NDK_HOME
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_HOME/build-tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+export MACOSX_DEPLOYMENT_TARGET=15.0
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -L/opt/homebrew/opt/llvm/lib/unwind -lunwind"
+export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
