990e68c84bce52f810627401e4ff336525368ced
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-12 21:57:21 +0900

    fix(lazyvim): grouping option

--- dotfiles/lazyvim/lua/config/options.lua
+++ dotfiles/lazyvim/lua/config/options.lua
@@ -2,20 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.editorconfig = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.g.editorconfig = true
 vim.g.autoformat = false
+vim.g.snacks_animate = false
+vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_cmp = "blink.cmp"
+vim.g.ai_cmp = false
 
 vim.opt.signcolumn = "no"
 vim.opt.numberwidth = 3
 vim.opt.mouse = ""
 vim.opt.list = false
 vim.opt.laststatus = 0
-
-vim.g.ai_cmp = false
-vim.g.snacks_animate = false
-
 vim.cmd([[
  se splitkeep=cursor
 ]])
