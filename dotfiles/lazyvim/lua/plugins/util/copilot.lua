0f0e45d68c483a7ece4ed3d39337be1963c1d4c6
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-13 17:19:03 +0900

    fix(lazyvim): change to certainty

--- dotfiles/lazyvim/lua/plugins/util/copilot.lua
+++ dotfiles/lazyvim/lua/plugins/util/copilot.lua
@@ -8,13 +8,8 @@ return {
         help = false,
         gitcommit = true,
         gitrebase = true,
-        sh = function()
-          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-            -- disable for .env files
-            return false
-          end
-          return true
-        end,
+
+        ["."] = false,
       },
     },
   },
