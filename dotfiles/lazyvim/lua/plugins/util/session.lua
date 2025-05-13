510a51378d6af55e4e9f3fd6771176e83e3f1e57
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-13 11:26:16 +0900

    fix(lazyvim): move to related file

--- dotfiles/lazyvim/lua/plugins/util/session.lua
+++ dotfiles/lazyvim/lua/plugins/util/session.lua
@@ -1,9 +1,12 @@
 return {
-  "olimorris/persisted.nvim",
-  lazy = false,
-  opts = {
-    autoload = false,
-    autosave = true,
-    use_git_branch = true,
+  { "folke/persistence.nvim", enabled = false },
+  {
+    "olimorris/persisted.nvim",
+    lazy = false,
+    opts = {
+      autoload = false,
+      autosave = true,
+      use_git_branch = true,
+    },
   },
 }
