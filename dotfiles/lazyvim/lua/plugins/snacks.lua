ad64bea845c76bf9393972c1945cccc1c2a6823d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-14 08:46:10 +0900

    fix(lazyvim): remove unnecessary plugins

--- dotfiles/lazyvim/lua/plugins/snacks.lua
+++ dotfiles/lazyvim/lua/plugins/snacks.lua
@@ -4,23 +4,14 @@ return {
     opts = {
       dashboard = {
         preset = {
-          header = [[
-        ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-        ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-        ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-        ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-        ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-        ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
- ]],
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = "󰊢 ", key = "g", desc = "Neogit", action = ":Neogit kind=tab" },
-          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = "󰊢 ", key = "g", desc = "Neogit", action = ":Neogit kind=tab" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
         },
       },
     },
--- dotfiles/lazyvim/lua/plugins/util/quick-scope.lua
+++ dotfiles/lazyvim/lua/plugins/util/quick-scope.lua
@@ -3,11 +3,11 @@ return {
   init = function()
     vim.g.qs_highlight_on_keys = { "f", "F", "t", "T" }
     vim.cmd([[
-    augroup qs_colors
-    autocmd!
-    autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
-    autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
-    augroup END
+      augroup qs_colors
+      autocmd!
+      autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+      autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+      augroup END
     ]])
   end,
 }
