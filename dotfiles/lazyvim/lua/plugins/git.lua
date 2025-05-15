b68c67257b692df423fff437ccacd34b81af5c30
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-15 11:30:14 +0900

    feat(lazyvim): Better git.lua

--- dotfiles/lazyvim/lua/plugins/git.lua
+++ dotfiles/lazyvim/lua/plugins/git.lua
@@ -25,30 +25,40 @@ return {
     "sindrets/diffview.nvim",
     opts = function()
       local actions = require("diffview.actions")
+
+      local next = { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } }
+      local prev = {
+        "n",
+        "<C-p>",
+        actions.select_prev_entry,
+        { desc = "Open the diff for the previous file" },
+      }
+      local close = { "n", "q", "<cmd>tabclose<CR>", { desc = "Close Diffview" } }
+      local commit = { "n", "c", "<cmd>Neogit commit<CR>", { desc = "Neogit Commit" } }
+
       return {
+        hooks = {
+          diff_buf_read = function()
+            vim.opt_local.wrap = false
+            vim.opt_local.list = false
+            vim.opt_local.colorcolumn = { 80 }
+            vim.opt_local.signcolumn = "no"
+            vim.opt_local.relativenumber = false
+            vim.opt_local.number = false
+          end,
+        },
         keymaps = {
           view = {
-            { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-            {
-              "n",
-              "<C-p>",
-              actions.select_prev_entry,
-              { desc = "Open the diff for the previous file" },
-            },
+            next,
+            prev,
+            close,
+            commit,
           },
           file_panel = {
-            {
-              "n",
-              "<C-n>",
-              actions.select_next_entry,
-              { desc = "Open the diff for the next file" },
-            },
-            {
-              "n",
-              "<C-p>",
-              actions.select_prev_entry,
-              { desc = "Open the diff for the previous file" },
-            },
+            next,
+            prev,
+            close,
+            commit,
           },
         },
       }
