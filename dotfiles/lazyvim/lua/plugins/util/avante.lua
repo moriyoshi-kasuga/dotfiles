ee01939aceda38c6bbb77b7470efd3a1e67a6c10
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-28 11:51:04 +0900

    feat(lazyvim): use avante instead of CopilotChat

---
+++ dotfiles/lazyvim/lua/plugins/util/avante.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      "saghen/blink.compat",
+    },
+    opts = {
+      sources = {
+        compat = {
+          "avante_commands",
+          "avante_mentions",
+          "avante_files",
+        },
+      },
+    },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    opts = {
+      provider = "copilot",
+      -- copilot = {
+      --   model = "claude-3.5-sonnet",
+      -- },
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true,
+      },
+      windows = {
+        position = "right",
+        wrap = true,
+        width = 30,
+      },
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
