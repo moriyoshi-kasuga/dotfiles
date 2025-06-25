f3e4dd10f471cea613ff13748074546202d8878b
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-25 11:17:38 +0900

    feat(lazyvim): add keymap of Diffview File History (All)

--- dotfiles/lazyvim/lua/plugins/git.lua
+++ dotfiles/lazyvim/lua/plugins/git.lua
@@ -66,6 +66,7 @@ return {
     keys = {
       { "<leader>gd", "<cmd>DiffviewOpen<CR>", desc = "Diffview Open" },
       { "<leader>gf", "<cmd>DiffviewFileHistory %<CR>", desc = "Diffview File History" },
+      { "<leader>gF", "<cmd>DiffviewFileHistory<CR>", desc = "Diffview File History (All)" },
     },
   },
 }
