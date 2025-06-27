3710f6e4cc254fb9fd33d36e9bca2761b67b1a0d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-27 22:48:27 +0900

    fix(zsh): remove unneccesary aliases

--- dotfiles/zsh/alias.zsh
+++ dotfiles/zsh/alias.zsh
@@ -27,24 +27,13 @@ alias dcbnc='docker compose build --no-cache'
 alias dl='lazydocker'
 
 # git
-alias ga="git add"
 alias gbd="git branch -d"
 alias gb='git branch'
 alias gcm='git commit -m'
-alias gca="git commit --amend"
 alias gc="git checkout"
 alias gcb="git checkout -b"
 alias gl="git log --date=iso --decorate"
-alias glp="git log --date=iso --decorate --patch"
-alias glg="git log --date=iso --graph --decorate --oneline --all"
 alias gs="git status"
-alias gf="git fetch"
-alias gp="git pull"
-alias gP="git push"
-alias gst="git stash"
-alias gstl="git stash list"
-alias gstp="git stash pop"
-alias gstd="git stash drop"
 
 # useful
 alias lg="lazygit"
