0c3944f0e2d1f0443c4bad9080c09e6806150dad
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-27 23:11:21 +0900

    fix(lazyvim): unneccesary number

--- dotfiles/lazyvim/lua/plugins/neo-tree.lua
+++ dotfiles/lazyvim/lua/plugins/neo-tree.lua
@@ -61,12 +61,12 @@ return {
         }
 
         vim.ui.select({
-          "1. Absolute path: " .. results[1],
-          "2. Path relative to CWD: " .. results[2],
-          "3. Path relative to HOME: " .. results[3],
-          "4. Filename: " .. results[4],
-          "5. Filename without extension: " .. results[5],
-          "6. Extension of the filename: " .. results[6],
+          "Absolute path: " .. results[1],
+          "Path relative to CWD: " .. results[2],
+          "Path relative to HOME: " .. results[3],
+          "Filename: " .. results[4],
+          "Filename without extension: " .. results[5],
+          "Extension of the filename: " .. results[6],
         }, { prompt = "Choose to copy to clipboard:" }, function(choice)
           if choice then
             local i = tonumber(choice:sub(1, 1))
