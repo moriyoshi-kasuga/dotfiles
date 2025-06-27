fd470310fc721788b0cb5f2699ff6bd4228c491e
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-06-27 22:14:58 +0900

    style: format indent

--- dotfiles/zsh/script/user.sh
+++ dotfiles/zsh/script/user.sh
@@ -1,95 +1,95 @@
 #!/usr/bin/env bash
 
 dcr() {
-    if [ $# -eq 0 ]; then
-        dcd && dcud && dcl -f
-    else
-        dcd "$1" && dcud "$1" && dcl "$1" -f
-    fi
+  if [ $# -eq 0 ]; then
+    dcd && dcud && dcl -f
+  else
+    dcd "$1" && dcud "$1" && dcl "$1" -f
+  fi
 }
 
 dcrb() {
-    if [ $# -eq 0 ]; then
-        dcd && dcudb && dcl -f
-    else
-        dcd "$1" && dcudb "$1" && dcl "$1" -f
-    fi
+  if [ $# -eq 0 ]; then
+    dcd && dcudb && dcl -f
+  else
+    dcd "$1" && dcudb "$1" && dcl "$1" -f
+  fi
 }
 
 useful() {
-    # if exist useful.sh
-    if [[ -f ./useful.sh ]]; then
-        # shellcheck disable=1091
-        source ./useful.sh "$@"
-        return
-    fi
-    touch ./useful.sh
-    echo "#!/usr/bin/env bash" >>./useful.sh
-    echo "" >>./useful.sh
-    chmod +x ./useful.sh
+  # if exist useful.sh
+  if [[ -f ./useful.sh ]]; then
+    # shellcheck disable=1091
+    source ./useful.sh "$@"
+    return
+  fi
+  touch ./useful.sh
+  echo "#!/usr/bin/env bash" >>./useful.sh
+  echo "" >>./useful.sh
+  chmod +x ./useful.sh
 }
 
 tn() {
-    args="$*"
-    if [ -z "$args" ]; then
-        tmux new -s "$(basename "$(pwd)")"
-        return
-    else
-        tmux new -s "$args"
-    fi
+  args="$*"
+  if [ -z "$args" ]; then
+    tmux new -s "$(basename "$(pwd)")"
+    return
+  else
+    tmux new -s "$args"
+  fi
 }
 
 tl() {
-    if [ $# -eq 0 ]; then
-        tmux ls
-        return
-    fi
-    local list
-    list="$(
-        IFS='|'
-        echo "$*"
-    )"
-    tmux list-sessions -F '#{session_name}' | grep -iE "$list"
+  if [ $# -eq 0 ]; then
+    tmux ls
+    return
+  fi
+  local list
+  list="$(
+    IFS='|'
+    echo "$*"
+  )"
+  tmux list-sessions -F '#{session_name}' | grep -iE "$list"
 }
 
 ta() {
-    if [ $# -eq 0 ]; then
-        tmux a
-        return
-    fi
-    tmux a -t "$(tl "$@")"
+  if [ $# -eq 0 ]; then
+    tmux a
+    return
+  fi
+  tmux a -t "$(tl "$@")"
 }
 
 ts() {
-    if [ $# -eq 0 ]; then
-        echo "Please input session name (type tl on print session list)"
-        return
-    fi
-    tmux switch -t "$(tl "$@")"
+  if [ $# -eq 0 ]; then
+    echo "Please input session name (type tl on print session list)"
+    return
+  fi
+  tmux switch -t "$(tl "$@")"
 }
 
 tk() {
-    if [ $# -eq 0 ]; then
-        tmux kill-session
-        return
-    fi
-    tmux kill-session -t "$(tl "$@")"
+  if [ $# -eq 0 ]; then
+    tmux kill-session
+    return
+  fi
+  tmux kill-session -t "$(tl "$@")"
 }
 
 cht() {
-    curl -s "cht.sh/${*}" | less -R
+  curl -s "cht.sh/${*}" | less -R
 }
 
 ide() {
-    tmux split-window -l 15
-    sleep 0.2
-    tmux split-window -h -l 66%
-    sleep 0.2
-    tmux split-window -h -l 50%
+  tmux split-window -l 15
+  sleep 0.2
+  tmux split-window -h -l 66%
+  sleep 0.2
+  tmux split-window -h -l 50%
 }
 
 mkcd() {
-    mkdir "$1" && cd "$1" && pwd
+  mkdir "$1" && cd "$1" && pwd
 }
 
 ggc() {
