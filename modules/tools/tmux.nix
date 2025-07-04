05dd4af9ffddf5c5a8c5bb90504e694cc06c9611
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-04 13:28:08 +0900

    feat: add tmux

--- modules/tools/tmux.nix
+++ modules/tools/tmux.nix
@@ -7,5 +7,11 @@
 
   programs.tmux = {
     enable = true;
+    baseIndex = 1;
+    escapeTime = 0;
+    clock24 = true;
+    keyMode = "vi";
+    shortcut = "C-t";
+    extraConfig = builtins.readFile ./tmux.conf;
   };
 }
