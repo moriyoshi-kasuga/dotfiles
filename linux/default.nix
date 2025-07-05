85a17bbd21695df46d163a263bc5a9eae7fd6dae
moriyoshi-kasuga <--local>
2025-07-05 22:08:01 +0900

    feat(linux): add clipboard integration for WSL

--- linux/default.nix
+++ linux/default.nix
@@ -1,3 +1,12 @@
-{
+{ pkgs, ... }:
 
+{
+  home.packages = with pkgs; [
+    (writeShellScriptBin "pbpaste" ''
+      win32yank.exe -o --lf
+    '')
+    (writeShellScriptBin "pbcopy" ''
+      win32yank.exe -i --crlf
+    '')
+  ];
 }
