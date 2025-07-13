a4991f0963c3b554a9ee44d85c14ed9eab1e7215
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-13 15:53:37 +0900

    feat(tools): update tff to 3.0.0

--- modules/tools/tff.nix
+++ modules/tools/tff.nix
@@ -4,21 +4,21 @@
   home.packages = [
     (pkgs.rustPlatform.buildRustPackage rec {
       pname = "TaggedFileFlow";
-      version = "2.0.0";
+      version = "3.0.0";
 
       src = pkgs.fetchFromGitHub {
         owner = "moriyoshi-kasuga";
         repo = "TaggedFileFlow";
         rev = "v${version}";
-        hash = "sha256-ibVIZnA9z2HSW1EXLA7qEcGK332Jy08yYBp+LFmGsv8=";
+        hash = "sha256-TrKhemh+ZL51v//x24CYritSACtSjpomHFNYH2pn7z8=";
       };
 
       useFetchCargoVendor = true;
-      cargoHash = "sha256-OiKmOCHAjVT9GuEHbr2GGaYazoaKN1wVscnoylRoL9Y=";
+      cargoHash = "sha256-a4kAnj+t0027k2nEeEBG1BZXqjjdY5gHYGomZCSoHfE=";
     })
   ];
 
   programs.zsh.initContent = ''
-    eval "$(tagged_file_flow init)"
+    eval "$(tagged_file_flow init zsh)"
   '';
 }
