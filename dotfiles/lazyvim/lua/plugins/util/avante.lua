0729821bb32f0363d7d1d9557132d92abb989801
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-07-28 19:50:07 +0900

    fix(lazyvim): disable copilot

--- dotfiles/lazyvim/lua/plugins/util/avante.lua
+++ dotfiles/lazyvim/lua/plugins/util/avante.lua
@@ -1,63 +1,63 @@
 return {
-  {
-    "Kaiser-Yang/blink-cmp-avante",
-    lazy = true,
-    specs = {
-      {
-        "saghen/blink.cmp",
-        optional = true,
-        opts = {
-          sources = {
-            default = { "avante" },
-            providers = { avante = { module = "blink-cmp-avante", name = "Avante" } },
-          },
-        },
-      },
-    },
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = true,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      provider = "copilot",
-      providers = {
-        copilot = {
-          model = "claude-3.7-sonnet",
-        },
-      },
-      behaviour = {
-        auto_suggestions = false,
-        auto_set_highlight_group = true,
-        auto_set_keymaps = true,
-        auto_apply_diff_after_generation = false,
-        support_paste_from_clipboard = false,
-        minimize_diff = true,
-      },
-      windows = {
-        position = "right",
-        wrap = true,
-        width = 30,
-      },
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "folke/snacks.nvim", -- for input provider snacks
-      "echasnovski/mini.icons",
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  -- {
+  --   "Kaiser-Yang/blink-cmp-avante",
+  --   lazy = true,
+  --   specs = {
+  --     {
+  --       "saghen/blink.cmp",
+  --       optional = true,
+  --       opts = {
+  --         sources = {
+  --           default = { "avante" },
+  --           providers = { avante = { module = "blink-cmp-avante", name = "Avante" } },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "yetone/avante.nvim",
+  --   event = "VeryLazy",
+  --   lazy = true,
+  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+  --   opts = {
+  --     provider = "copilot",
+  --     providers = {
+  --       copilot = {
+  --         model = "claude-3.7-sonnet",
+  --       },
+  --     },
+  --     behaviour = {
+  --       auto_suggestions = false,
+  --       auto_set_highlight_group = true,
+  --       auto_set_keymaps = true,
+  --       auto_apply_diff_after_generation = false,
+  --       support_paste_from_clipboard = false,
+  --       minimize_diff = true,
+  --     },
+  --     windows = {
+  --       position = "right",
+  --       wrap = true,
+  --       width = 30,
+  --     },
+  --   },
+  --   build = "make",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --     "nvim-lua/plenary.nvim",
+  --     "MunifTanjim/nui.nvim",
+  --     --- The below dependencies are optional,
+  --     "folke/snacks.nvim", -- for input provider snacks
+  --     "echasnovski/mini.icons",
+  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       "MeanderingProgrammer/render-markdown.nvim",
+  --       opts = {
+  --         file_types = { "markdown", "Avante" },
+  --       },
+  --       ft = { "markdown", "Avante" },
+  --     },
+  --   },
+  -- },
 }
