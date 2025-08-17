5673f3dd407d0283885965b3f8ab9c6dffdc6dd2
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-17 13:52:02 +0900

    feat(lazyvim): add avante.nvim

--- dotfiles/lazyvim/lua/plugins/util/avante.lua
+++ dotfiles/lazyvim/lua/plugins/util/avante.lua
@@ -1,63 +1,75 @@
 return {
-  -- {
-  --   "Kaiser-Yang/blink-cmp-avante",
-  --   lazy = true,
-  --   specs = {
-  --     {
-  --       "saghen/blink.cmp",
-  --       optional = true,
-  --       opts = {
-  --         sources = {
-  --           default = { "avante" },
-  --           providers = { avante = { module = "blink-cmp-avante", name = "Avante" } },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "yetone/avante.nvim",
-  --   event = "VeryLazy",
-  --   lazy = true,
-  --   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-  --   opts = {
-  --     provider = "copilot",
-  --     providers = {
-  --       copilot = {
-  --         model = "claude-3.7-sonnet",
-  --       },
-  --     },
-  --     behaviour = {
-  --       auto_suggestions = false,
-  --       auto_set_highlight_group = true,
-  --       auto_set_keymaps = true,
-  --       auto_apply_diff_after_generation = false,
-  --       support_paste_from_clipboard = false,
-  --       minimize_diff = true,
-  --     },
-  --     windows = {
-  --       position = "right",
-  --       wrap = true,
-  --       width = 30,
-  --     },
-  --   },
-  --   build = "make",
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter",
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --     --- The below dependencies are optional,
-  --     "folke/snacks.nvim", -- for input provider snacks
-  --     "echasnovski/mini.icons",
-  --     "zbirenbaum/copilot.lua", -- for providers='copilot'
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       "MeanderingProgrammer/render-markdown.nvim",
-  --       opts = {
-  --         file_types = { "markdown", "Avante" },
-  --       },
-  --       ft = { "markdown", "Avante" },
-  --     },
-  --   },
-  -- },
+  {
+    "yetone/avante.nvim",
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
+    event = "VeryLazy",
+    version = false,
+    opts = {
+      provider = "copilot",
+      hints = {
+        enabled = false,
+      },
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true,
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "zbirenbaum/copilot.lua",
+      { "echasnovski/mini.pick", optional = true }, -- for file_selector provider mini.pick
+      { "nvim-telescope/telescope.nvim", optional = true }, -- for file_selector provider telescope
+      { "hrsh7th/nvim-cmp", optional = true }, -- autocompletion for avante commands and mentions
+      { "ibhagwan/fzf-lua", optional = true }, -- for file_selector provider fzf
+      { "stevearc/dressing.nvim", optional = true }, -- for input provider dressing
+      { "folke/snacks.nvim", optional = true }, -- for input provider snacks
+      { "nvim-tree/nvim-web-devicons", optional = true }, -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        optional = true,
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        optional = true,
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+      {
+        "saghen/blink.cmp",
+        optional = true,
+        dependencies = {
+          "Kaiser-Yang/blink-cmp-avante",
+        },
+        opts = {
+          sources = {
+            default = { "avante" },
+            providers = { avante = { module = "blink-cmp-avante", name = "Avante" } },
+          },
+        },
+      },
+    },
+  },
 }
