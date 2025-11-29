190c069c142080c46f6943472086080c88cbc5c5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-11-30 08:24:23 +0900

    feat(nix): add some package for using neovim

--- home/editor/neovim.nix
+++ home/editor/neovim.nix
@@ -2,9 +2,13 @@
   config,
   lib,
   vars,
+  pkgs,
   ...
 }:
 
+let
+  treesitterGrammars = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+in
 {
   catppuccin.nvim.enable = false;
 
@@ -16,6 +20,29 @@
 
   programs.neovim = {
     enable = true;
+
+    extraWrapperArgs = [
+      "--set"
+      "TREESITTER_GRAMMARS"
+      "${treesitterGrammars}"
+    ];
+
+    extraPackages = with pkgs; [
+      treesitterGrammars
+
+      lua-language-server
+      nixd
+      efm-langserver
+      pyright
+      typos-lsp
+      ruff
+      stylua
+      hadolint
+      actionlint
+      svelte-language-server
+      tailwindcss-language-server
+      vtsls
+    ];
   };
 
   programs.zsh.shellAliases = {
