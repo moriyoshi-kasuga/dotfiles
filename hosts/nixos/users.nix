a4ea507e49f2d5a5b3b4030ff26ab2e362a1b839
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-07 14:29:53 +0900

    fix(nix): correct settings thunar

--- hosts/nixos/users.nix
+++ hosts/nixos/users.nix
@@ -29,12 +29,19 @@
       aseprite # ピクセルアートエディタ
       ldtk # レベルデザインツールキット
 
-      # Gui File Explorer
-      thunar
       # Minecraft Launcher
       prismlauncher
     ];
   };
+
+  programs.thunar.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+  programs.xfconf.enable = true;
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
 
   # Zsh シェルの有効化
   programs.zsh.enable = true;
