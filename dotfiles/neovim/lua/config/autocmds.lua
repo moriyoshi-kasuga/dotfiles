68c680775cc04d354ab8b01d2179336ca6c23060
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-07 09:33:12 +0900

    feat(neovim): use snacks rename on oil.nvim

--- dotfiles/neovim/lua/config/autocmds.lua
+++ dotfiles/neovim/lua/config/autocmds.lua
@@ -8,3 +8,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     (vim.hl or vim.highlight).on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OilActionsPost",
+  callback = function(event)
+    if event.data.actions[1].type == "move" then
+      Snacks.rename.on_rename_file(event.data.actions[1].src_url, event.data.actions[1].dest_url)
+    end
+  end,
+})
