95228421cbc2131ca9801ba812062df0cdf7639f
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-11 09:17:35 +0900

    feat(neovim): use lazydev.nvim

--- dotfiles/neovim/after/lsp/lua_ls.lua
+++ dotfiles/neovim/after/lsp/lua_ls.lua
@@ -7,14 +7,6 @@ return {
         pathStrict = true,
         path = { "?.lua", "?/init.lua" },
       },
-      workspace = {
-        library = vim.list_extend(vim.api.nvim_get_runtime_file("lua", true), {
-          "${3rd}/luv/library",
-          "${3rd}/busted/library",
-          "${3rd}/luassert/library",
-        }),
-        checkThirdParty = false,
-      },
       codeLens = {
         enable = true,
       },
