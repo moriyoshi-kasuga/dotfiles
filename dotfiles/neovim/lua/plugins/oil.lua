f33982c60ba4708f87c71cdd97c59cdd017464cf
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-15 21:32:07 +0900

    fix(neovim): show dot files and ignore vcs

--- dotfiles/neovim/lua/plugins/oil.lua
+++ dotfiles/neovim/lua/plugins/oil.lua
@@ -2,6 +2,49 @@ if require("config.util").is_in_simple_mode() then
   return {}
 end
 
+-- ref: https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#hide-gitignored-files-and-show-git-tracked-hidden-files
+
+-- helper function to parse output
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      -- Remove trailing slash
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+-- build git status cache
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+local git_status = new_git_status()
+
 return {
   {
     "stevearc/oil.nvim",
@@ -15,12 +58,31 @@ return {
         autosave_changes = true,
         timeout_ms = 1000,
       },
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          local dir = require("oil").get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then
+            return is_dotfile
+          end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
+        is_always_hidden = function(name, _)
+          return name == "target" or name == "node_modules" or name == ".git"
+        end,
+      },
       keymaps = {
         ["g?"] = { "actions.show_help", mode = "n" },
         ["<CR>"] = "actions.select",
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = { "actions.close", mode = "n" },
-        ["<C-l>"] = "actions.refresh",
         ["<C-/>"] = {
           function()
             local _, oil_dir = pcall(require("oil").get_current_dir)
@@ -45,6 +107,17 @@ return {
       },
       use_default_keymaps = false,
     },
+    config = function(_, opts)
+      require("oil").setup(opts)
+
+      -- Clear git status cache on refresh
+      local refresh = require("oil.actions").refresh
+      local orig_refresh = refresh.callback
+      refresh.callback = function(...)
+        git_status = new_git_status()
+        orig_refresh(...)
+      end
+    end,
     keys = {
       { "<leader>e", "<cmd>Oil<CR>", desc = "Open Oil file browser" },
       { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
