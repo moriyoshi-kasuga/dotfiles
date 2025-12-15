d138bac3389724daf1cfeb713306f9ba97eea99f
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-15 17:40:28 +0900

    feat(nix): add gamemode pkg

--- hosts/nixos/gaming.nix
+++ hosts/nixos/gaming.nix
@@ -1,8 +1,23 @@
 {
+  pkgs,
+  vars,
+  ...
+}:
+
+{
   # Steam ゲーミングプラットフォーム
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
+  };
+
+  users.users.${vars.username} = {
+    extraGroups = [
+      "gamemode"
+    ];
+    packages = with pkgs; [
+      gamemode
+    ];
   };
 }
