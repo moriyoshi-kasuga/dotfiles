6991ade86bab122ec3cd4d470bb254f675759c20
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-17 12:11:04 +0900

    fix(neovim): statuscolumn redering

--- dotfiles/neovim/lua/config/statuscolumn.lua
+++ dotfiles/neovim/lua/config/statuscolumn.lua
@@ -10,7 +10,9 @@ _G.StatusColumnFn = function()
     number = vim.v.lnum
   end
 
-  local max_line = vim.fn.line("$")
+  local win = vim.g.statusline_winid
+  local buf = vim.api.nvim_win_get_buf(win)
+  local max_line = vim.api.nvim_buf_line_count(buf)
   local digits = math.max(#tostring(max_line), 3)
   return highlight .. string.format("%" .. digits .. "s ", number)
 end
