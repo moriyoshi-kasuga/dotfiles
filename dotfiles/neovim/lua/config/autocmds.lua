9cfe822c1abced34f4b493b39264429f8a3275f1
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-20 12:31:14 +0900

    feat(neovim): better tab

--- dotfiles/neovim/lua/config/autocmds.lua
+++ dotfiles/neovim/lua/config/autocmds.lua
@@ -11,30 +11,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- Skip these autocmds in simple mode
-if not is_simple_mode then
-  -- go to last loc when opening a buffer
-  vim.api.nvim_create_autocmd("BufReadPost", {
-    group = augroup("last_loc"),
-    callback = function(event)
-      local exclude = { "gitcommit" }
-      local buf = event.buf
-      if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
-        return
-      end
-      vim.b[buf].last_loc = true
-      local mark = vim.api.nvim_buf_get_mark(buf, '"')
-      local lcount = vim.api.nvim_buf_line_count(buf)
-      if mark[1] > 0 and mark[1] <= lcount then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-      end
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "rust" },
-    callback = function()
-      vim.keymap.set("i", "'", "'", { buffer = true })
-    end,
-  })
+if is_simple_mode then
+  return
 end
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].last_loc then
+      return
+    end
+    vim.b[buf].last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
