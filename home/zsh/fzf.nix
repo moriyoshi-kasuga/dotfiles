151c166a0e8d45745b463e179f326c8430c692e9
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-25 18:59:41 +0900

    fix(tools): adjust fzf default opts

--- home/zsh/fzf.nix
+++ home/zsh/fzf.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
 
+let
+  defaultOptions = [
+    "--reverse"
+    "--style minimal"
+    "--tmux 55%,65%"
+  ];
+  opts = pkgs.lib.concatStringsSep " " defaultOptions;
+in
 {
   home.packages = with pkgs; [
     fzf
@@ -8,14 +16,17 @@
   programs = {
     fzf = {
       enable = true;
+      enableZshIntegration = true;
+      inherit defaultOptions;
     };
   };
 
-  programs.zsh.initContent = ''
-    export FZF_DEFAULT_OPTS='--style minimal --tmux 75%,85%'
-    export FZF_DEFAULT_COMMAND='fd --hidden --type l --type f --type d --exclude .git --exclude .cache';
-    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  home.sessionVariables = {
+    _ZO_FZF_OPTS = opts;
+    FZF_DEFAULT_COMMAND = "fd --hidden --type l --type f --type d --exclude .git --exclude .cache";
+  };
 
+  programs.zsh.initContent = ''
     zstyle ':fzf-tab:*' popup-min-size 75 12
     zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
     zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
