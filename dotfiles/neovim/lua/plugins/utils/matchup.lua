a096065c41387f0b24c3abe01302748168523708
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-12-30 10:42:42 +0900

    fix(neovim): disable matchup status

--- dotfiles/neovim/lua/plugins/utils/matchup.lua
+++ dotfiles/neovim/lua/plugins/utils/matchup.lua
@@ -5,5 +5,6 @@ return {
     -- NOTE: disable markdown because so lag
     -- ref: https://github.com/andymass/vim-matchup/issues/416
     vim.g.matchup_treesitter_disabled = { "markdown" }
+    vim.g.matchup_matchparen_status_offscreen = 0
   end,
 }
