d4172e899db1e386341ca0a11a0d8efc899519a7
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-01-04 20:39:55 +0900

    fix(neovim): disable vim-matchup on haskell

--- dotfiles/neovim/lua/plugins/utils/matchup.lua
+++ dotfiles/neovim/lua/plugins/utils/matchup.lua
@@ -2,9 +2,14 @@ return {
   "andymass/vim-matchup",
   event = { "BufReadPost", "BufNewFile", "BufWritePost" },
   init = function()
-    -- NOTE: disable markdown because so lag
-    -- ref: https://github.com/andymass/vim-matchup/issues/416
-    vim.g.matchup_treesitter_disabled = { "markdown" }
+    vim.g.matchup_treesitter_disabled = {
+      -- NOTE: disable markdown because so lag
+      -- ref: https://github.com/andymass/vim-matchup/issues/416
+      "markdown",
+
+      -- highlight is noisy
+      "haskell",
+    }
     vim.g.matchup_matchparen_status_offscreen = 0
   end,
 }
