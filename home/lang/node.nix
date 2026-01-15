34df0dc25def1df6c38a27afcf18d76fd178d226
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-01-15 22:04:16 +0900

    feat(nix): use direct node instead of volta

--- home/lang/node.nix
+++ home/lang/node.nix
@@ -2,14 +2,11 @@
 
 {
   home.packages = with pkgs; [
-    volta
+    nodejs_latest
     bun
   ];
 
   programs.zsh.envExtra = ''
-    export VOLTA_HOME="$HOME/.volta"
-    export PATH="$VOLTA_HOME/bin:$PATH"
-
     export PATH="$HOME/.bun/bin:$PATH"
   '';
 }
