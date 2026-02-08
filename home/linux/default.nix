985614a7f872f5997281bfcc169a628df24d3ee2
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-02-08 19:57:35 +0900

    feat(linux): zathura config

--- home/linux/default.nix
+++ home/linux/default.nix
@@ -26,4 +26,52 @@
     clang
     gcc-unwrapped
   ];
+
+  programs.zathura = {
+    enable = true;
+    # ref: https://sheepla.github.io/sheepla-note/posts/zathura-introduction/
+    extraConfig = ''
+      # ズームイン・スームアウトやスクロールの段階
+      set zoom-step 20
+      set scroll-step 80
+
+      # クリップボードを有効にする
+      set selection-clipboard clipboard
+
+      # インクリメンタル検索を有効にする
+      set incremental-search true
+
+      # キーバインド
+      map u scroll half-up
+      map d scroll half-down
+      map D toggle_page_mode
+      map K zoom in
+      map J zoom out
+
+      # ステータスバーに表示されるファイルパスのホームディレクトリを ~ に変更
+      set statusbar-home-tilde true
+
+      # ウインドウタイトルをファイルのbasenameにする
+      set window-title-basename true
+
+      # UI要素の表示・非表示
+      set guioptions cshv
+
+      # UIのフォント
+      set font "monospace 12"
+
+      # 1ページ表示で起動（二分割しない）
+      set pages-per-row 1
+      set first-page-column 1
+
+      # ページ表示の調整
+      set adjust-open "best-fit"
+      set scroll-page-aware true
+      set scroll-full-overlap 0.01
+      set recolor-keephue true
+      set recolor-reverse-video true
+      set render-loading false
+      set window-title-page true
+    '';
+  };
 }
--- hosts/nixos/users.nix
+++ hosts/nixos/users.nix
@@ -28,7 +28,6 @@
       ldtk
       prismlauncher
       imhex
-      zathura
     ];
   };
 
