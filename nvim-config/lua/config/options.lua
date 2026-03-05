25a06c55c8a1f8604811f80342b12c24a70eb505
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-03-05 18:59:36 +0900

    fix(neovim): change lsp.log to flatten in /tmp

--- nvim-config/lua/config/options.lua
+++ nvim-config/lua/config/options.lua
@@ -1,6 +1,6 @@
 local is_simple_mode = require("config.util").is_in_simple_mode()
 
-vim.lsp.log._set_filename("/tmp/nvim/lsp.log")
+vim.lsp.log._set_filename("/tmp/nvim-lsp.log")
 
 vim.opt.backup = true
 local backup_dir = vim.fn.expand(vim.fn.stdpath("cache") .. "/.vim_backup")
