a4af245eb449a0fbae3e9659453eb2e1516add13
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-04-19 17:02:52 +0900

    fix(nixos): move lib to library.nix

--- modules/common/library.nix
+++ modules/common/library.nix
@@ -19,10 +19,13 @@ mkModule {
     {
       modules.library.libs = with pkgs; [
         ffmpeg
+        curl
         openssl
+        libssh
         sqlite
         icu
         lua5_4
+        pkg-config
       ];
       home.packages = cfg.libs;
       home.sessionVariables = rec {
@@ -42,14 +45,51 @@ mkModule {
         LIBRARY_PATH = LD_LIBRARY_PATH;
       };
     };
-  linuxHomeModule = {
-
-  };
   darwinHomeModule = {
     modules.library.libs = with pkgs; [
       libiconv
       llvm
       nsis
+    ];
+  };
+  nixosModule = {
+    environment.systemPackages = with pkgs; [
+      pkg-config
+    ];
+  };
+  linuxHomeModule = {
+    modules.library.libs = with pkgs; [
+      zlib
+      zstd
+      stdenv.cc.cc
+      attr
+      bzip2
+      libxml2
+      acl
+      libsodium
+      util-linux
+      xz
+
+      # My own additions
+      libxcomposite
+      libxtst
+      libxrandr
+      libxext
+      libx11
+      libxfixes
+      libxkbcommon
+      libxi
+      libGL
+      libva
+      pipewire
+      libxcb
+      libxdamage
+      libxshmfence
+      libxxf86vm
+      libelf
+      udev
+      librsvg
+      webkitgtk_4_1
     ];
   };
 }
--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,46 +1,5 @@
 { username, pkgs, ... }:
 
-let
-  # LD related
-  library = with pkgs; [
-    pkg-config
-    # List by default
-    zlib
-    zstd
-    stdenv.cc.cc
-    curl
-    openssl
-    attr
-    libssh
-    bzip2
-    libxml2
-    acl
-    libsodium
-    util-linux
-    xz
-    systemd
-
-    # My own additions
-    libxcomposite
-    libxtst
-    libxrandr
-    libxext
-    libx11
-    libxfixes
-    libxkbcommon
-    libxi
-    libGL
-    libva
-    pipewire
-    libxcb
-    libxdamage
-    libxshmfence
-    libxxf86vm
-    libelf
-    ffmpeg
-    udev
-  ];
-in
 {
   users.users.${username} = {
     isNormalUser = true;
@@ -72,38 +31,16 @@ in
     gid = 1000;
   };
 
-  # System-wide packages
-  environment.systemPackages =
-    with pkgs;
-    [
-      vim-full
-      wget
-      appimage-run
-    ]
-    ++ library;
-
   # Nix-LD
   programs.nix-ld = {
     enable = true;
-    libraries = library;
   };
 
-  environment.sessionVariables = {
-    # ref: https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/programs/nix-ld.nix#L42
-    LD_LIBRARY_PATH = "/run/current-system/sw/share/nix-ld/lib";
-
-    PKG_CONFIG_PATH =
-      let
-        getPkgConfigPath =
-          pkg:
-          let
-            path = "${pkg.dev or pkg}/lib/pkgconfig";
-          in
-          if builtins.pathExists path then path else null;
-        pkgConfigPaths = builtins.filter (x: x != null) (map getPkgConfigPath library);
-      in
-      pkgs.lib.concatStringsSep ":" pkgConfigPaths;
-  };
+  environment.systemPackages = with pkgs; [
+    vim-full
+    wget
+    appimage-run
+  ];
 
   # Gaming
   programs.steam = {
