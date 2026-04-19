07463d5a15146c7e11a93c29d5cb6be0b2ae1523
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-04-19 13:37:40 +0900

    fix(nixos): fcitx5 settings

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -71,6 +71,37 @@
       addons = with pkgs; [
         fcitx5-mozc
       ];
+      settings = {
+        globalOptions = {
+          "Hotkey" = {
+            TriggerKeys = "";
+          };
+          "Hotkey/ActivateKeys" = {
+            "0" = "Alt+Alt_R";
+          };
+          "Hotkey/DeactivateKeys" = {
+            "0" = "Alt+Alt_L";
+          };
+        };
+        inputMethod = {
+          GroupOrder = {
+            "0" = "Default";
+          };
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "mozc";
+            Layout = "";
+          };
+        };
+      };
     };
   };
   catppuccin.fcitx5.enable = true;
