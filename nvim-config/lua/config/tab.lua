e0c276c0de230d1a9aaf4d26f722b34ebb6d30f5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-05-04 22:10:04 +0900

    fix(neovim): tab settings

--- nvim-config/lua/config/tab.lua
+++ nvim-config/lua/config/tab.lua
@@ -4,37 +4,49 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 ---@class TabOpt
+---@field extensions string[]
 ---@field width integer
 ---@field expandtab false? (default true)
 
----@type table<string, TabOpt>
+---@type TabOpt[]
 local tab = {
-  rust = {
+  {
+    extensions = {
+      "rust",
+      "python",
+      "java",
+      "kotlin",
+      "scala",
+      "swift",
+      "c",
+      "cpp",
+      "cs",
+      "php",
+      "haskell",
+      "elixir",
+      "zig",
+      "nim",
+      "just",
+      "dosbatch",
+      "sql",
+    },
     width = 4,
   },
-  python = {
-    width = 4,
-  },
-  make = {
-    width = 8,
-    expandtab = false,
-  },
-  just = {
-    width = 4,
-  },
-  dosbatch = {
-    width = 4,
-  },
-  go = {
-    width = 4,
-    expandtab = false,
-  },
+  { extensions = { "go" }, width = 4, expandtab = false },
+  { extensions = { "make" }, width = 8, expandtab = false },
 }
+
+local lookup = {}
+for _, opt in ipairs(tab) do
+  for _, ext in ipairs(opt.extensions) do
+    lookup[ext] = opt
+  end
+end
 
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup("mori_auto_tabset_per_filetype", { clear = true }),
   callback = function(event)
-    local opt = tab[vim.bo[event.buf].filetype]
+    local opt = lookup[vim.bo[event.buf].filetype]
     if opt == nil then
       return
     end
