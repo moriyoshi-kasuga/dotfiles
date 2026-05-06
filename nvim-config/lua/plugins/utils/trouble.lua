74643042f50f525aded3cfa9168f685c33d2b755
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-05-07 01:51:36 +0900

    feat(neovim): add trouble.nvim

---
+++ nvim-config/lua/plugins/utils/trouble.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = { focus = true },
+    -- stylua: ignore
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>cl", "<cmd>Trouble lsp toggle<cr>", desc = "LSP References (Trouble)" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+  },
+}
