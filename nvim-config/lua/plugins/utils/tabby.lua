90d6bbb2bf53878a01d93efa00051222f925a5c5
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-05-30 00:53:28 +0900

    feat(neovim): add tabby.lua

---
+++ nvim-config/lua/plugins/utils/tabby.lua
@@ -0,0 +1,37 @@
+return {
+  "nanozuki/tabby.nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+  },
+  event = "UIEnter",
+  config = function()
+    local palette = require("catppuccin.palettes").get_palette("mocha")
+
+    local theme = {
+      fill   = { fg = palette.surface2, bg = "none" },
+      active = { fg = palette.mauve, bg = palette.surface0, style = "bold" },
+      normal = { fg = palette.overlay1, bg = "none" },
+    }
+
+    require("tabby").setup({
+      line = function(line)
+        return {
+          line.tabs().foreach(function(tab)
+            local hl = tab.is_current() and theme.active or theme.normal
+            return {
+              " ",
+              tab.number(),
+              " ",
+              tab.name(),
+              tab.close_btn(" "),
+              " ",
+              hl = hl,
+            }
+          end),
+          line.spacer(),
+          hl = theme.fill,
+        }
+      end,
+    })
+  end,
+}
