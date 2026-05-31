c13cdd4b73d47d411a759afce777f42798a5a003
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-06-01 00:33:08 +0900

    feat(rust): add rustup

--- modules/common/lang/rust.nix
+++ modules/common/lang/rust.nix
@@ -8,7 +8,17 @@ mkModule {
   name = "lang.rust";
   inheritModule = "lang";
   homeModule = {
+    programs.zsh.initContent = ''
+      export PATH="$PATH:~/.cargo/bin"
+    '';
+
+    programs.fish.interactiveShellInit = ''
+      fish_add_path ~/.cargo/bin
+    '';
+
     home.packages = with pkgs; [
+      rustup
+
       cargo-msrv
       cargo-nextest
       cargo-udeps
