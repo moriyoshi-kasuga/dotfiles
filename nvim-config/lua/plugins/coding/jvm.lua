62f10a8b231dfd10b3747b6e132849fa80411033
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-06-06 14:06:38 +0900

    fix(neovim): work jdtls

--- nvim-config/lua/plugins/coding/jvm.lua
+++ nvim-config/lua/plugins/coding/jvm.lua
@@ -80,5 +80,11 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     ft = "java",
+    config = function()
+      require("jdtls").start_or_attach({
+        cmd = { "jdtls" },
+        root_dir = vim.fs.root(0, { "gradlew", "build.gradle", "build.gradle.kts", ".git", "mvnw" }),
+      })
+    end,
   },
 }
