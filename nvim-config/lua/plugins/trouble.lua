888c226f43b88d776414771172f36981d0f7373d
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-06-12 22:25:42 +0900

    fix(neovim): split trouble.nvim into file

---
+++ nvim-config/lua/plugins/trouble.lua
@@ -0,0 +1,27 @@
+if require("config.util").is_in_simple_mode() then
+  return {}
+end
+
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = { focus = true },
+    -- stylua: ignore
+    keys = {
+      -- diagnostics
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      -- symbols / LSP
+      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>cl", "<cmd>Trouble lsp toggle<cr>", desc = "LSP Definitions/References (Trouble)" },
+      { "gR", "<cmd>Trouble lsp_references toggle<cr>", desc = "References (Trouble)" },
+      -- list
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+      -- todo-comments.nvim
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+    },
+  },
+}
