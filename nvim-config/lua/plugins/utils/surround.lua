90619e14de5ae0f174ffc9545777539f33337a3c
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-07-16 16:27:42 +0900

    feat(nvim): add generic surround

--- nvim-config/lua/plugins/utils/surround.lua
+++ nvim-config/lua/plugins/utils/surround.lua
@@ -2,5 +2,44 @@ return {
   "kylechui/nvim-surround",
   version = "^4.0.0",
   event = "VeryLazy",
-  opts = {},
+  config = function()
+    local config = require("nvim-surround.config")
+    local opts = {
+      surrounds = {
+        ["g"] = {
+          add = function()
+            local result = config.get_input("Enter the Type name: ")
+            if result then
+              return { { result .. "<" }, { ">" } }
+            end
+          end,
+          find = function()
+            if vim.g.loaded_nvim_treesitter then
+              local selection = config.get_selection({
+                query = {
+                  capture = "@call.outer",
+                  type = "textobjects",
+                },
+              })
+              if selection then
+                return selection
+              end
+            end
+            return config.get_selection({ pattern = "[^=%s%<%>{}]+%b<>" })
+          end,
+          delete = "^(.-%<)().-(%>)()$",
+          change = {
+            target = "^.-([%w_]+)()%<.-%>()()$",
+            replacement = function()
+              local result = config.get_input("Enter the Type name: ")
+              if result then
+                return { { result }, { "" } }
+              end
+            end,
+          },
+        },
+      },
+    }
+    require("nvim-surround").setup(opts)
+  end,
 }
