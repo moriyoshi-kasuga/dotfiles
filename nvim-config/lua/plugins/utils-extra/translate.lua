1a19c0e6fe581cb79e277b602ad4f04c3eabe5a3
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2026-07-16 11:37:22 +0900

    feat(nvim): add translate.lua

---
+++ nvim-config/lua/plugins/utils-extra/translate.lua
@@ -0,0 +1,11 @@
+return {
+  "uga-rosa/translate.nvim",
+  opts = {
+    preset = {
+      output = {},
+    },
+  },
+  keys = {
+    { "<leader>ar", "<cmd>Translate ja<CR>", mode = { "v" } },
+  },
+}
